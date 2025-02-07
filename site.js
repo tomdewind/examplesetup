bc6f5248c5f22e4e322dd559bc38b125204b264e
Tom <tom@dewind.nl>
2025-02-07 12:55:06 +0100

    added box and octagon buttons

--- site.js
+++ site.js
@@ -1,1 +1,14 @@
-alert('hello world')
+const box = document.getElementById('box');
+const octagon = document.getElementById('octagon');
+
+console.log(box);
+box.addEventListener("click", (event) => {alert('hoi')});
+
+console.log(octagon);
+octagon.addEventListener("click", (event) => {alert('gegroet')});
+
+// luister naar een click event
+
+// waneer je klikt, zegt hoi
+
+//alert('hoi');
